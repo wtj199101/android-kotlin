751de951acec894e42cf89afba6c501fc91bdb9a
wtj199101 <422434668@qq.com>
2019-09-23 09:51:58 +0800

    update project

--- app/src/main/java/com/example/myapplication2/demo1/Person.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication2.demo1
-
-open class Animal(name: String)
-
-class Person (name: String,userName: String) : Animal(name){
-    fun add(x: Int,y: Int ): Int= x+y
-
-}
---
+++ kotlinStudy/src/main/java/com/yunfan/kotlinstudy/BaseAcitivity.kt
@@ -0,0 +1,10 @@
+package com.yunfan.kotlinstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.AnkoLogger
+
+/**
+ * 通用的基类
+ */
+open class BaseAcitivity : AppCompatActivity(),AnkoLogger
+
