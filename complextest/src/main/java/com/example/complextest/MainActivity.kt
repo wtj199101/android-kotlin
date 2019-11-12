7635dd90dc7b14fcddc94c98fc66d39e02c0fe70
wtj199101 <422434668@qq.com>
2019-11-12 17:04:29 +0800

    11122

--- complextest/src/main/java/com/example/complextest/MainActivity.kt
+++ complextest/src/main/java/com/example/complextest/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.complextest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.complextest.activity.SpinnerDialogActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_spinner_dialog.setOnClickListener { startActivity<SpinnerDialogActivity>() }
     }
 }
