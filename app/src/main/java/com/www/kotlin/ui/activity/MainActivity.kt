0bd4ffa7f8b89f354b1ecfd4712d5388c0902af7
wtj199101 <422434668@qq.com>
2020-01-07 14:58:37 +0800

    update_mainfile

--- app/src/main/java/com/www/kotlin/ui/activity/MainActivity.kt
+++ app/src/main/java/com/www/kotlin/ui/activity/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.www.kotlin.ui.activity
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.cxz.kotlin.baselibs.base.BaseActivity
+import com.google.android.material.navigation.NavigationView
 import com.www.kotlin.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    private var navController: NavController?=null
 
     override fun init(savedInstanceState: Bundle?) {
 
